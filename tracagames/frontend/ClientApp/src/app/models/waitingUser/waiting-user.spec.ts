9ab76b5e31ea0d937c0c10a6496489aeead5b6ab
Antonio Ramón Padrón Ferrer <antonio.antoniopf@gmail.com>
2022-11-12 22:14:03 +0000

    Se crea el pipeline para azure y también la tarea de github que disparará el pipeline en azure

--- tracagames/frontend/ClientApp/src/app/models/waitingUser/waiting-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { WaitingUser } from './waiting-user';
-
-describe('WaitingUser', () => {
-  it('should create an instance', () => {
-    // expect(new WaitingRoomUser()).toBeTruthy();
-  });
-});
